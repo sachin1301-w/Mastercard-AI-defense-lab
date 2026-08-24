93e48c25e57956ad08b10ebeecf57eba7e610c9a
SACHIN KUMAR <sk.39648215@gmail.com>
2026-08-24 18:27:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,17 +30,17 @@ JWT_SECRET = os.environ.get(
 TOKEN_LIFETIME_SECONDS = 8 * 60 * 60
 
 app = Flask(__name__)
-CORS(
-    app,
-    resources={r"/*": {"origins": [
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://mastercard-ai-defense-lab.vercel.app"
-    ]}},
-    allow_headers=["Content-Type", "Authorization"],
-    methods=["GET", "POST", "OPTIONS"]
-)
-
+# CORS(
+#     app,
+#     resources={r"/*": {"origins": [
+#         "http://localhost:5173",
+#         "http://127.0.0.1:5173",
+#         "https://mastercard-ai-defense-lab.vercel.app"
+#     ]}},
+#     allow_headers=["Content-Type", "Authorization"],
+#     methods=["GET", "POST", "OPTIONS"]
+# )
+CORS(app)
 
 def get_db():
     connection = sqlite3.connect(DATABASE_PATH)
